6da74542c35d7a1c14702fd0b587fedc109e6ff1
aneesh.chan@gmail.com <aneesh.chan@gmail.com>
2025-12-31 12:40:24 -0600

    Fix: Remove aggressive outputFileTracingExcludes that broke Vercel serverless

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Skip output file tracing to avoid race condition in build
-  outputFileTracingExcludes: {
-    '*': ['**/*'],
-  },
 }
 
 module.exports = nextConfig
